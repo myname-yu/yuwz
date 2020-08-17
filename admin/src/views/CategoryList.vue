55b3405f7a579ae07088bbbbcfeb37ba2b020544
myname-yu <2219023140@qq.com>
2020-08-17 20:31:54 +0800

    第二次

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -2,42 +2,58 @@
   <div>
     <h1>分类列表</h1>
     <el-table :data="items">
-        <el-table-column prop="_id" label="ID" width='260'>
-        </el-table-column>
-          <el-table-column prop="name" label="分类名称" >
-        </el-table-column>
-         <el-table-column
-      fixed="right"
-      label="操作"
-      width="100">
-      <template slot-scope="scope">
-        <el-button type="text" size="small" @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
-      </template>
-    </el-table-column>
-      </el-table>
+      <el-table-column prop="_id" label="ID" width="260"></el-table-column>
+      <el-table-column prop="name" label="分类名称"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button
+            type="text"
+            size="small"
+            @click="$router.push(`/categories/edit/${scope.row._id}`)"
+          >编辑</el-button>
+          <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-  items:[]
-    }
+      items: [],
+    };
   },
   methods: {
-    async fetch(){
-// await的意思就是等待。它后面可以跟一个表达式。如果是值(如字符串、数字、普通对象等等)的话，返回值就是本身的值。
-// 不过最常用的是后面跟一个promise对象。await会等待这个promise的状态由pending转为fulfilled或者rejected。在此期间它会阻塞，延迟执行await语句后面的语句。
-// 如果promise对象的结果是resolve，它会将resolve的值，作为await表达式的运算结果。
-     this.$http.get('categories').then(res=>{
+    async fetch() {
+      // await的意思就是等待。它后面可以跟一个表达式。如果是值(如字符串、数字、普通对象等等)的话，返回值就是本身的值。
+      // 不过最常用的是后面跟一个promise对象。await会等待这个promise的状态由pending转为fulfilled或者rejected。在此期间它会阻塞，延迟执行await语句后面的语句。
+      // 如果promise对象的结果是resolve，它会将resolve的值，作为await表达式的运算结果。
+      this.$http.get("categories").then((res) => {
         console.log(res);
-      this.items=res.data
+        this.items = res.data;
       });
-     
-    }
+    },
+    // 列表删除功能
+    async remove(row) {
+      console.log(row);
+      console.log(row.name, row._id);
+      this.$confirm(`是否确定要删除分类“${row.name}”?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(async () => {
+        const res = await this.$http.delele(`categories/${row._id}`);
+        console.log(res);
+        this.$message({
+          type: "success",
+          message: "删除成功!",
+        });
+      });
+    },
   },
   created() {
-    this.fetch()
+    this.fetch();
   },
-}
+};
 </script>
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -7,15 +7,28 @@ module.exports = app => {
         const model = await Category.create(req.body);
         res.send(model)
     });
+    // 列表编辑
+    router.put('/categories/:id', async(req, res) => {
+        const model = await Category.findOneAndUpdate(req.params.id, req.body);
+        res.send(model)
+    });
     // 分类列表
     router.get('/categories', async(req, res) => {
         const items = await Category.find().limit(10);
         res.send(items)
     });
-    // 列表编辑
+    // 点击编辑之后，在编辑分类页显示要编辑的内容
     router.get('/categories/:id', async(req, res) => {
         const model = await Category.findById(req.params.id);
         res.send(model)
-    })
+    });
+    // 删除分类
+    router.delete('/categories/:id', async(req, res) => {
+        // await Category.findByIdAndDelete(req.params.id, req.body);
+        // res.send({
+        //     success: true
+        // })
+        console.log('删除');
+    });
     app.use('/admin/api', router)
 }
